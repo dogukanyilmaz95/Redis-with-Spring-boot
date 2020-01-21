83f4e1e84238fcb70c3f56361bb6fa34b4129960
dogukan.yilmaz <dodo.123>
2020-01-21 16:57:28 +0300

    first commit

---
+++ src/main/java/com/dogukanyilmaz/redisspringboot/config/RedisConfig.java
@@ -0,0 +1,77 @@
+package com.dogukanyilmaz.redisspringboot.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.Cache;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author dogukanyilmaz
+ * Created 21.01.2020
+ */
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class RedisConfig extends CachingConfigurerSupport implements CachingConfigurer {
+
+    @Value(value = "${redis.host}")
+    private String host;
+    private Integer port = 6379;
+    private Integer expirationTimeout = 36000000;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+        redisConnectionFactory.setHostName(host);
+        redisConnectionFactory.setPort(port);
+        redisConnectionFactory.setTimeout(expirationTimeout);
+        return redisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(@Autowired RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
+
+    @Override
+    public CacheErrorHandler errorHandler() {
+        return new RedisCacheErrorHandler();
+    }
+
+    @Slf4j
+    public static class RedisCacheErrorHandler implements CacheErrorHandler {
+
+        @Override
+        public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+            log.info("Unable to get from cache " + cache.getName() + " : " + exception.getMessage());
+        }
+
+        @Override
+        public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+            log.info("Unable to put into cache " + cache.getName() + " : " + exception.getMessage());
+        }
+
+        @Override
+        public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+            log.info("Unable to evict from cache " + cache.getName() + " : " + exception.getMessage());
+        }
+
+        @Override
+        public void handleCacheClearError(RuntimeException exception, Cache cache) {
+            log.info("Unable to clean cache " + cache.getName() + " : " + exception.getMessage());
+        }
+    }
+
+}
